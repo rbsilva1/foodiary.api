8328609a9f08aedba2456449169486b20342e50f
rbsilva1 <rodrigo.belarmino17@gmail.com>
2025-07-19 16:00:43 -0300

    feat: GetMealById task creation

---
+++ src/controllers/get-meal-by-id.controller.ts
@@ -0,0 +1,42 @@
+import { and, eq } from "drizzle-orm";
+import z from "zod";
+import { db } from "../db";
+import { mealsTable } from "../db/schema";
+import type { HttpResponse, ProtectedHttpRequest } from "../types/http.type";
+import { badRequest, ok } from "../utils/http";
+
+const schema = z.object({
+	mealId: z.uuid()
+});
+
+export class GetMealByIdController {
+	async handle({
+		userId,
+		params,
+	}: ProtectedHttpRequest): Promise<HttpResponse> {
+		const result = schema.safeParse(params);
+
+		if (!result.success) {
+			return badRequest({
+				errors: result.error.issues,
+			});
+		}
+
+		const [meal] = await db.query.mealsTable.findMany({
+			where: and(
+				eq(mealsTable.userId, userId),
+				eq(mealsTable.id, result.data.mealId),
+			),
+			columns: {
+				id: true,
+				foods: true,
+				createdAt: true,
+				icon: true,
+				name: true,
+				status: true,
+			},
+		});
+
+		return ok({ meal });
+	}
+}
